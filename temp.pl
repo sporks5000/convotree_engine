4f3acfc3d2bd6215a3302a3358d0ab834aadda2c
acwilliams <acwilliams@liquidweb.com>
2022-08-17 21:18:18 -0400

    initial commit

---
+++ temp.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env /home/username/perl5/bin/plackup
+
+BEGIN {
+	push @INC, "$ENV{HOME}/perl5/lib/perl5";
+};
+
+use strict;
+use warnings;
+
+use Plack::Request;
+use JSON;
+
+my $app = sub {
+	my $env = shift;
+
+	my $request = Plack::Request->new($env);
+	my $body = eval {
+		JSON::decode_json($request->raw_body || '{}');
+	} || {};
+	my $html;
+	if ($body->{pid}) {
+		$html = $$;
+	}
+	else {
+		my $name = $body->{name} || $request->raw_body;
+		$html = "Hello $name\n";
+	}
+
+	return [
+		'200',
+		['Content-Type' => 'text/html; charset=utf8'],
+		[$html],
+	];
+};
+
